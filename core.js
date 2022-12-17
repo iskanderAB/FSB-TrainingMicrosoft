f8c2d01d21d4c195491619cd5ac9b72d42f4e279
iskanderab <iskanderabasi@gmail.com>
2022-12-17 10:32:51 +0100

    fix remove function

--- core.js
+++ core.js
@@ -4,15 +4,24 @@ const inpt = document.querySelector('#inpt');
 
 const btn = document.querySelector("#btn");
 
+
 const list = document.getElementById('list');
 
 
+
+const deletefn = (count) =>{
+    document.getElementById(`li-${count}`).style.display= 'none';
+}
+
+let count = 0 ; 
 const addToTodo = () => {
     if( inpt.value != '' ){
-        list.innerHTML += `<li class='todo'> <span>${inpt.value}</span> <strong> </strong> </li>`;
+        list.innerHTML += `<li id='li-${count}'  class='todo'> <span >${inpt.value}</span> <strong onclick='deletefn(${count})' id="str-${count}"> </strong> </li>`;        
         inpt.value='';
+        count++
     }
 }
+
 
 btn.addEventListener('click',addToTodo);
 
